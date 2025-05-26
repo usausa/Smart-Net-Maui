425404846ebaae2281adb75d3eb42727ff5c46cb
usausa <machi.pon@gmail.com>
2025-05-26 18:58:53 +0900

    Update settings

--- Smart.Maui.Extensions/Assembly.cs
+++ Smart.Maui.Extensions/Assembly.cs
@@ -1,7 +1,5 @@
 [assembly: CLSCompliant(false)]
 
-[assembly: XmlnsDefinition("http://smart/maui", "Smart.Maui")]
-[assembly: XmlnsDefinition("http://smart/maui", "Smart.Maui.Animations")]
 [assembly: XmlnsDefinition("http://smart/maui", "Smart.Maui.Data")]
 [assembly: XmlnsDefinition("http://smart/maui", "Smart.Maui.Interactivity")]
 [assembly: XmlnsDefinition("http://smart/maui", "Smart.Maui.Markup")]
