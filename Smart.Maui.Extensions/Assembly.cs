90709613bbd52bf32a16299c46f3c7015316ff2c
usausa <machi.pon@gmail.com>
2025-05-26 18:53:19 +0900

    Update setting

--- Smart.Maui.Extensions/Assembly.cs
+++ Smart.Maui.Extensions/Assembly.cs
@@ -1,1 +1,10 @@
 [assembly: CLSCompliant(false)]
+
+[assembly: XmlnsDefinition("http://smart/maui", "Smart.Maui")]
+[assembly: XmlnsDefinition("http://smart/maui", "Smart.Maui.Animations")]
+[assembly: XmlnsDefinition("http://smart/maui", "Smart.Maui.Data")]
+[assembly: XmlnsDefinition("http://smart/maui", "Smart.Maui.Interactivity")]
+[assembly: XmlnsDefinition("http://smart/maui", "Smart.Maui.Markup")]
+[assembly: XmlnsDefinition("http://smart/maui", "Smart.Maui.Resolver")]
+[assembly: XmlnsDefinition("http://smart/maui", "Smart.Maui.Validation")]
+[assembly: Microsoft.Maui.Controls.XmlnsPrefix("http://smart/maui", "s")]
--- Smart.Maui/Assembly.cs
+++ Smart.Maui/Assembly.cs
@@ -1,1 +1,8 @@
 [assembly: CLSCompliant(false)]
+
+[assembly: XmlnsDefinition("http://smart/maui", "Smart.Maui")]
+[assembly: XmlnsDefinition("http://smart/maui", "Smart.Maui.Animations")]
+[assembly: XmlnsDefinition("http://smart/maui", "Smart.Maui.Data")]
+[assembly: XmlnsDefinition("http://smart/maui", "Smart.Maui.Interactivity")]
+[assembly: XmlnsDefinition("http://smart/maui", "Smart.Maui.Markup")]
+[assembly: Microsoft.Maui.Controls.XmlnsPrefix("http://smart/maui", "s")]
