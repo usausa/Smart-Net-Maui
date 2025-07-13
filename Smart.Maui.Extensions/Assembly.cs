85d78406beff34fcfc37c9de2d4682e8d78e29e3
usausa <machi.pon@gmail.com>
2025-07-13 19:22:03 +0900

    Update expression

--- Smart.Maui.Extensions/Assembly.cs
+++ Smart.Maui.Extensions/Assembly.cs
@@ -1,8 +1,6 @@
 [assembly: CLSCompliant(false)]
 
-[assembly: XmlnsDefinition("http://smart/maui", "Smart.Maui.Data")]
 [assembly: XmlnsDefinition("http://smart/maui", "Smart.Maui.Interactivity")]
-[assembly: XmlnsDefinition("http://smart/maui", "Smart.Maui.Markup")]
 [assembly: XmlnsDefinition("http://smart/maui", "Smart.Maui.Resolver")]
 [assembly: XmlnsDefinition("http://smart/maui", "Smart.Maui.Validation")]
 [assembly: Microsoft.Maui.Controls.XmlnsPrefix("http://smart/maui", "s")]
