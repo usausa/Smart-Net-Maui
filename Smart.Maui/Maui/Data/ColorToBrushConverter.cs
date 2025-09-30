7e1f54a7a7696638ef04a51f38019cc250106aad
usausa <machi.pon@gmail.com>
2025-09-30 09:31:18 +0900

    Update converter

---
+++ Smart.Maui/Maui/Data/ColorToBrushConverter.cs
@@ -0,0 +1,16 @@
+namespace Smart.Maui.Data;
+
+using System.Globalization;
+
+public sealed class ColorToBrushConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value is Color color ? new SolidColorBrush(color) : null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value is SolidColorBrush brush ? brush.Color : null;
+    }
+}
