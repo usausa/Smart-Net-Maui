41b8296a29325f2ca9537e15b10831820fe4b45e
usausa <machi.pon@gmail.com>
2026-07-25 14:04:00 +0900

    Update

--- Smart.Maui/Maui/Data/EnumDescriptionConverter.cs
+++ Smart.Maui/Maui/Data/EnumDescriptionConverter.cs
@@ -1,5 +1,6 @@
 namespace Smart.Maui.Data;
 
+using System.Collections.Concurrent;
 using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
@@ -7,7 +8,8 @@ using System.Reflection;
 
 public sealed class EnumDescriptionConverter : IValueConverter
 {
-    [UnconditionalSuppressMessage("Trimming", "IL2075", Justification = "Enum type members are expected to be preserved by the application")]
+    private static readonly ConcurrentDictionary<(Type, object), string> Cache = new();
+
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         if (value is null)
@@ -15,21 +17,29 @@ public sealed class EnumDescriptionConverter : IValueConverter
             return null;
         }
 
-        if (value is Enum enumValue)
+        if (value is Enum)
         {
-            var type = enumValue.GetType();
-            var mis = type.GetMember(enumValue.ToString());
-            if (mis.Length > 0)
-            {
-                var attr = mis[0].GetCustomAttribute<DescriptionAttribute>();
-                if (attr is not null)
-                {
-                    return attr.Description;
-                }
-            }
+            return Cache.GetOrAdd((value.GetType(), value), static key => GetDescription(key.Item2));
         }
 
         return value.ToString();
+    }
+
+    [UnconditionalSuppressMessage("Trimming", "IL2075", Justification = "Enum type members are expected to be preserved by the application")]
+    private static string GetDescription(object value)
+    {
+        var type = value.GetType();
+        var mis = type.GetMember(value.ToString()!);
+        if (mis.Length > 0)
+        {
+            var attr = mis[0].GetCustomAttribute<DescriptionAttribute>();
+            if (attr is not null)
+            {
+                return attr.Description;
+            }
+        }
+
+        return value.ToString()!;
     }
 
     public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
